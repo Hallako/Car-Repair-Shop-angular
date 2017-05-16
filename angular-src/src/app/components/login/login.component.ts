fac10a6443c2a0a093015812b0339fe02c5d9b95
Hallako <Toni.saario@hotmail.fi>
2017-05-16 12:17:58 +0300

    Calender-update

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -33,15 +33,14 @@ export class LoginComponent implements OnInit {
         this.authService.storeUserData(data.token, data.user);
         this.flashMessage.show('you are now logged in', {
           cssClass: 'alert-success',
-          timeout: 5000});
-
+          timeout: 3000});
+          this.router.navigate(['dashboard']);
       } else {
         this.flashMessage.show(data.msg, {
           cssClass: 'alert-danger',
-          timeout: 5000});
+          timeout: 3000});
         this.router.navigate(['login']);
       }
   });
-
   }
 }
