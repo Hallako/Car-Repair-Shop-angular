303d8d325ade28ebe5abb5ebf2e0555d08d4b99a
PKaappari <panu.paakkari@outlook.com>
2017-05-16 09:16:05 +0300

    Added redirect
    added redirect to login page

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
@@ -16,7 +18,8 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService:AuthService,
               private router:Router,
-              private flashMessage:FlashMessagesService
+              private flashMessage:FlashMessagesService,
+              private location: Location
               ) { }
 
   ngOnInit() {
@@ -27,14 +30,14 @@ export class LoginComponent implements OnInit {
           username: this.username,
           password: this.password
       }
-      
+
   this.authService.authenticateUser(user).subscribe(data => {
       if(data.success){
         this.authService.storeUserData(data.token, data.user);
         this.flashMessage.show('you are now logged in', {
           cssClass: 'alert-success',
-          timeout: 5000});
-
+          timeout: 5000})
+          this.router.navigate(['dashboard']);
       } else {
         this.flashMessage.show(data.msg, {
           cssClass: 'alert-danger',
