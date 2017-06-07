28553f8276065d73e89ea6c84050093470c5ce55
PKaappari <panu.paakkari@outlook.com>
2017-06-07 15:06:26 +0300

    Merge remote-tracking branch 'refs/remotes/Hallako/master' into limit
    # Conflicts:
    #	angular-src/src/app/components/dashboard/dashboard.component.ts

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -59,12 +59,6 @@ export class DashboardComponent implements OnInit {
             calEvent.backgroundColor = "#133313";
             this.calElement.fullCalendar( 'updateEvent', calEvent )
             calEvent.backgroundColor = tempcolor;
-<<<<<<< HEAD
-=======
-            this.calElement.fullCalendar('unselect');
-            this.calElement.fullCalendar('rerender');
-
->>>>>>> refs/remotes/Hallako/master
             if(calEvent.user){
               this.updatename(calEvent);
             } else {
@@ -76,14 +70,11 @@ export class DashboardComponent implements OnInit {
 
             this.end = moment(calEvent.end).format('YYYY-MM-DD[T]HH:mm');
             this.start = moment(calEvent.start).format('YYYY-MM-DD[T]HH:mm');
-<<<<<<< HEAD
             this.onTitleChange()
             this.calElement.fullCalendar('unselect')
             this.calElement.fullCalendar('renrender')
           }
-=======
-            this.onTitleChange();
->>>>>>> refs/remotes/Hallako/master
+
         };
 
         let boundClick = clickFunc.bind(this);
@@ -160,7 +151,7 @@ export class DashboardComponent implements OnInit {
 
                 this.calElement.fullCalendar('rerenderEvents');
 
-                
+
                 if(res >= 2) {
                   this.flashMessage.show('Et voi varata yli 2 päällekkäistä tapahtumaa', {cssClass: 'alert-danger', timeout:3000});
                   this.calElement.fullCalendar('unselect');
