7d3497ba383be4a31d1ee8a9ee3ef71a18895272
Hallako <Toni.saario@hotmail.fi>
2017-06-15 14:29:55 +0300

    3

--- routes/chat.js
+++ routes/chat.js
@@ -2,23 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+
+
 var Chat = require('../models/Chat.js');
 
 //server.listen(4000);
-
-// socket io
-io.on('connection', function(socket) {
-    console.log('User connected');
-    socket.on('disconnect', function() {
-        console.log('User disconnected');
-    });
-    socket.on('save-message', function(data) {
-        console.log(data);
-        io.emit('new-message', { message: data });
-    });
-});
 
 /* GET ALL CHATS */
 router.get('/:room', function(req, res, next) {
