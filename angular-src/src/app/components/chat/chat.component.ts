2766db51fab6aac327ee69563d62e6cf247d68de
Hallako <Toni.saario@hotmail.fi>
2017-06-16 13:51:32 +0300

    Update

--- angular-src/src/app/components/chat/chat.component.ts
+++ angular-src/src/app/components/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
+import { AuthService } from '../../services/auth.service';
 import * as io from "socket.io-client";
 
 
@@ -13,37 +14,54 @@ export class ChatComponent implements OnInit, AfterViewChecked {
 
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
-
+  Admin: boolean;
   Hidden: boolean;
   chats: any;
   joinned: boolean = false;
   newUser = { nickname: '', room: '' };
   msgData = { room: '', nickname: '', message: '' };
-  socket = io('');
+  socket = io('http://localhost:8081/');
 
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService,
+              private authservice: AuthService) {}
 
   ngOnInit() {
 
+    this.Admin = this.authservice.getAdmin();
+    console.log(this.Admin)
+    
     this.newUser.nickname = 'Asiakaspalvelu '+JSON.parse(localStorage.getItem("user")).name;
-
     var user = JSON.parse(localStorage.getItem("userr"));
 
-    if(user!==null) {
+    if(user !== null) {
       if(user.room){
-      this.getChatByRoom(user.room);
-      this.msgData = { room: user.room, nickname: user.nickname, message: '' }
-      this.joinned = true;
-      this.scrollToBottom();
+        this.getChatByRoom(user.room);
+        this.msgData = { room: user.room, nickname: user.nickname, message: '' }
+        this.joinned = true;
+        this.scrollToBottom();
       }
     }
+
+    //###### SOCKETS ########
     this.socket.on('new-message', function (data) {
       if(data.message.room === JSON.parse(localStorage.getItem("userr")).room) {
         this.chats.push(data.message);
-        this.msgData = { room: user.room, nickname: user.nickname, message: '' }
+        
+        this.msgData = { room: this.newUser.room, nickname: user.nickname, message: '' }
         this.scrollToBottom();
       }
     }.bind(this));
+
+    this.socket.on('userconn-response', function (data) {
+      this.newUser.room = data.socketconnection;
+      this.msgData.room = data.socketconnection;
+
+      localStorage.setItem("userr", JSON.stringify(this.newUser));
+      this.getChatByRoom(this.newUser.room);
+      console.log(data.socketconnection)
+    }.bind(this));
+
+    //Ngoninit END
   }
 
   ngAfterViewChecked() {
@@ -64,18 +82,17 @@ export class ChatComponent implements OnInit, AfterViewChecked {
     });
   }
 
-  joinRoom() {
+  createRoom() {
     var date = new Date();
-    localStorage.setItem("userr", JSON.stringify(this.newUser));
-    this.getChatByRoom(this.newUser.room);
     this.msgData = { room: this.newUser.room, nickname: this.newUser.nickname, message: '' };
-    this.joinned = true;
-    this.socket.emit('save-message', { room: this.newUser.room, nickname: this.newUser.nickname, message: 'Join this room', updated_at: date });
+    this.socket.emit('createroom', { room: this.newUser.room, nickname: this.newUser.nickname, message: 'Join this room', updated_at: date });
+    this.joinned = true;  
   }
 
   sendMessage() {
     this.chatService.saveChat(this.msgData).then((result) => {
       this.socket.emit('save-message', result);
+      this.msgData.message = '';
     }, (err) => {
       console.log(err);
     });
@@ -84,12 +101,12 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   logout() {
     var date = new Date();
     var user = JSON.parse(localStorage.getItem("userr"));
-    this.socket.emit('save-message', { room: user.room, nickname: user.nickname, message: 'Left this room', updated_at: date });
+
+    this.socket.emit('adminleaveroom', { room: user.room, nickname: user.nickname, message: 'Left this room', updated_at: date });
     localStorage.removeItem("userr");
     this.newUser.room = "";
     this.joinned = false;
   }
-
 
   togglehide(){
     if(this.Hidden == true){
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
   nodeUrl: String;
 
   constructor(private http: Http) {
-    this.nodeUrl = '';
+    this.nodeUrl = 'http://localhost:8081/';
 
     if (this.user == null) {
       this.user = JSON.parse(localStorage.getItem('user'));
