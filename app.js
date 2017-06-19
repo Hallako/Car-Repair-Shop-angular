3448b66062d892945980121f607702c487e3198e
Apaitsol <t6paat00@students.oamk.fi>
2017-06-19 13:52:30 +0300

    .

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const http = require('http');
 const favicon = require('serve-favicon');
-
 const config = require('./config/database');
-
 const users = require('./routes/users');
 const events = require('./routes/events');
 const chat = require('./routes/chat');
 
+//Variables
+var SocketConnections = createArray(5, 3);
 
 //DB conf
 mongoose.connect(config.database);
@@ -22,13 +22,13 @@ mongoose.connect(config.database);
 mongoose.connection.on('connected', () => {
     console.log('connected to DB');
 });
-
 mongoose.connection.on('error', (err) => {
     console.log('connected to DB' + err);
 });
 
 const app = express();
 app.use(logger('dev'));
+
 //port
 const httpport = process.env.PORT || 8081;
 
@@ -44,7 +44,6 @@ app.use(bodyParser.json());
 //passport midware
 app.use(passport.initialize());
 app.use(passport.session());
-
 require('./config/passport')(passport);
 
 //routes
@@ -61,18 +60,113 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+//Start server
 var httpServer = http.createServer(app);
 httpServer.listen(httpport);
 
+//Start Socket.io
 var io = require('socket.io').listen(httpServer);
+
 // socket io
 io.on('connection', function(socket) {
+
     console.log('User connected');
-    socket.on('disconnect', function() {
-        console.log('User disconnected');
+
+    socket.on('admincreateroom', function(data) {
+
+
+        for (var i = 0; i < SocketConnections.length; i++) {
+            if (SocketConnections[i][0] == undefined) {
+
+                SocketConnections[i][0] = data.user;
+                SocketConnections[i][1] = true;
+                SocketConnections[i][2] = data.user;
+
+                socket.join(data.user);
+
+                io.emit('adminconn-response', { message: 'success' });
+
+                break;
+            }
+        }
+
     });
+
+    socket.on('userjoin', function(data) {
+
+
+        for (var i = 0; i < SocketConnections.length; i++) {
+            if (SocketConnections[i][1] == true) {
+
+                var roomid = data.user + SocketConnections[i][0];
+
+
+                socket.join(roomid);
+                io.in(SocketConnections[i][0]).emit('userconn-response', { message: data, available: true, room: roomid });
+
+                SocketConnections[i][0] = roomid;
+                SocketConnections[i][1] = false;
+                socket.emit('userconn-response', { message: data, available: true, room: roomid });
+
+                return;
+            }
+        }
+        io.emit('userconn-response', { message: data, available: false });
+    });
+
+    socket.on('adminleaveroom', function(data) {
+        var x = data.room;
+        var y = false;
+
+        io.in(data.room).emit('releasesocket', { room: data.room });
+
+        for (var k = 0; k < SocketConnections.length; k++) {
+            if (SocketConnections[k][0] == x && SocketConnections[k][1] == y) {
+
+                SocketConnections[k][0] = null;
+                SocketConnections[k][1] = null;
+                SocketConnections[k][2] = null;
+                break;
+            }
+        }
+
+    });
+
+    socket.on('userdisconnect', function(data) {
+        var x = data.room;
+        var y = false;
+
+        io.in(data.room).emit('userdc', { message: data });
+        socket.leave(data.room);
+        for (var k = 0; k < SocketConnections.length; k++) {
+            if (SocketConnections[k][0] == x && SocketConnections[k][1] == y) {
+                SocketConnections[k][1] = true;
+                SocketConnections[k][0] = SocketConnections[k][2];
+
+                break;
+            }
+        }
+    });
+
+
+    socket.on('disconnect', function(data) {
+
+    });
+
     socket.on('save-message', function(data) {
-        console.log(data);
+
         io.emit('new-message', { message: data });
     });
+
+    //######## SOCKETS END ###########
 });
+
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+    }
+    return arr;
+}
