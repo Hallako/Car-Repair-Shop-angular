c74e04ed5d5a8232ad27e8b07b9facbbd1d35778
Apaitsol <t6paat00@students.oamk.fi>
2017-06-27 09:47:05 +0300

    Kirjautuminen tehty uusiksi

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -13,46 +14,45 @@ import { Location } from '@angular/common';
 
 export class LoginComponent implements OnInit {
 
-  username: String;
-  password: String;
+  loginForm: FormGroup;
 
   constructor(private authService: AuthService,
     private router: Router,
     private flashMessage: FlashMessagesService,
-    private location: Location
-  ) { }
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = fb.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    })
+  }
 
   ngOnInit() {
   }
 
   onLoginSubmit() {
     const user = {
-      username: this.username,
-      password: this.password
+      username: this.loginForm.get('username').value,
+      password: this.loginForm.get('password').value
     }
-    if (this.username == undefined || this.password == undefined) {
-      this.flashMessage.show('Anna käyttäjänimi ja salasana!', {
-        cssClass: 'alert-danger',
-        timeout: 3000
-      })
-    }
-    else {
-      this.authService.authenticateUser(user).subscribe(data => {
-        if (data.success) {
-          this.authService.storeUserData(data.token, data.user);
-          this.flashMessage.show('you are now logged in', {
-            cssClass: 'alert-success',
-            timeout: 3000
-          });
-          this.router.navigate(['dashboard']);
-        } else {
-          this.flashMessage.show(data.msg, {
-            cssClass: 'alert-danger',
-            timeout: 3000
-          });
-          this.router.navigate(['login']);
-        }
-      });
-    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessage.show('Kirjautuminen onnistui', {
+          cssClass: 'alert-success',
+          timeout: 3000
+        });
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessage.show(data.msg, {
+          cssClass: 'alert-danger',
+          timeout: 3000
+        });
+        this.router.navigate(['login']);
+      }
+    });
   }
+
 }
