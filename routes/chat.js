619da6c19b7366a28e109591452875d9f2c58a67
Hallako <Toni.saario@hotmail.fi>
2017-07-18 11:50:47 +0300

    heroku

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Chat = require('../models/Chat.js');
 
-/* GET ALL CHATS */
+// GET ALL CHATS 
 router.get('/:room', function(req, res, next) {
     Chat.find({ room: req.params.room }, function(err, chats) {
         if (err) return next(err);
@@ -11,7 +11,7 @@ router.get('/:room', function(req, res, next) {
     });
 });
 
-/* SAVE CHAT */
+// SAVE CHAT
 router.post('/', function(req, res, next) {
     Chat.create(req.body, function(err, post) {
         if (err) return next(err);
